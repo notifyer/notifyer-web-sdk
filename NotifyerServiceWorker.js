4e5de385753d700c053af63911c7cf0a5a181540
Huseyin BABAL <huseyinbabal88@gmail.com>
2016-09-21 22:11:01 +0300

    Web sdk updated

--- NotifyerServiceWorker.js
+++ NotifyerServiceWorker.js
@@ -1,3 +1,29 @@
-if( 'function' === typeof window){
-    importScripts('https://cdn.notifyer.io/sdks/notifyerServiceWorker.js');
-}
+var notificationUrl = '';
+
+self.addEventListener('push', function (event) {
+    console.log("Push event: ", event.data.text());
+    var _data = event.data ? JSON.parse(event.data.text()) : {};
+    notificationUrl = _data.url;
+    event.waitUntil(
+        self.registration.showNotification(_data.title, {
+            body: _data.message,
+            icon: _data.icon,
+            tag: _data.tag
+        })
+    );
+});
+
+self.addEventListener('notificationclick', function (event) {
+    event.notification.close();
+
+    event.waitUntil(
+        clients.matchAll({
+            type: "window"
+        })
+            .then(function (clientList) {
+                if (clients.openWindow) {
+                    return clients.openWindow(notificationUrl);
+                }
+            })
+    );
+});
