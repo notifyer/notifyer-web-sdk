b7f98e15c0e3ab42524281fa939fe260eccbebb3
Huseyin BABAL <huseyinbabal88@gmail.com>
2016-10-13 00:08:54 +0300

    New version released

--- NotifyerServiceWorker.js
+++ NotifyerServiceWorker.js
@@ -3,7 +3,7 @@ var notificationUrl = '';
 self.addEventListener('push', function (event) {
     console.log("Push event: ", event.data.text());
     var _data = event.data ? JSON.parse(event.data.text()) : {};
-    notificationUrl = _data.url;
+    notificationUrl = (typeof _data.url === 'undefined' || _data.url == "")? "/": _data.url;
     event.waitUntil(
         self.registration.showNotification(_data.title, {
             body: _data.message,
